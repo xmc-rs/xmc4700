b15f5679bb7f34ca451fe6cbde2039b0c84d635f
Lucas Brendel <lucasbrendel@gmail.com>
2017-10-24 21:27:41 -0500

    Ran 'form' acrossed project

---
+++ src/sdmmc_con/mod.rs
@@ -0,0 +1,1 @@
+use vcell :: VolatileCell ; # [ doc = r" Register block" ] # [ repr ( C ) ] pub struct RegisterBlock { # [ doc = "0x00 - SDMMC Configuration" ] pub sdmmc_con : SDMMC_CON } # [ doc = "SDMMC Configuration" ] pub struct SDMMC_CON { register : VolatileCell < u32 > } # [ doc = "SDMMC Configuration" ] pub mod sdmmc_con ;
